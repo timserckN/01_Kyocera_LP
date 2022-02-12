22fea533a2a05567d7a0adc2c135411640117e5a
Timothée Serck <timserck@MacBook-Pro-de-Timothee.local>
2022-02-12 13:05:40 +0100

    test

--- scripts/form.js
+++ scripts/form.js
@@ -14,11 +14,10 @@ window.addEventListener("load", function() {
     };
     var pristine = new Pristine(form, defaultConfig);
     var honey = document.querySelector('[name="contact_me_by_fax_only"]').value;
+    console.log(honey, "honey");
     form.addEventListener("submit", function(e) {
-        console.log(e, "?e")
         e.preventDefault();
         var valid = pristine.validate();
-        console.log(valid, honey, "valid ?")
         if (valid && honey === 1) {
             const data = new FormData(form);
             const action = e.target.action;
@@ -28,7 +27,7 @@ window.addEventListener("load", function() {
                     body: data,
                 })
                 .then(() => {
-                    e.target.reset();
+
                     window.open("https://timserckn.github.io/01_Kyocera_LP/integrations/tk.html")
                     window.open("https://ftp.kyonet.fr/public/file/SGEshd-6Q0eswNQ2t1kbUw/KYO_Production_printing_FR_BD.pdf", "_blank")
                 })
